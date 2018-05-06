4c416f695aad35f6b98cd97780302add45a18f44
unknown <syyadkhan@yahoo.com>
2018-05-06 14:34:49 -0400

    may061434

--- app.js
+++ app.js
@@ -4,11 +4,18 @@ var app = express();
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res){
-    res.render("home");
+    res.render("landing");
 });
 
 
-
+app.get("/campgrounds", function(req, res){
+    var campgrounds = [
+        {name: "big hill", image: "https://cdn.pixabay.com/photo/2017/08/06/02/32/camp-2587926_960_720.jpg"},
+        {name: "small hill", image: "https://www.campcheerio.org/images/home-slides/IMG_3046_revised.jpg"},
+        {name: "my hill", image: "http://www.rapidfitkc.com/wp-content/uploads/2017/09/Best-Boot-Camp-in-Kansas-city.jpg"}
+    ]
+    res.render("campgrounds");
+});
 
 
 
