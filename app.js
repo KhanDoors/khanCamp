7fe2e62dfdd216baccb5c2f10fc32d43060e3758
unknown <syyadkhan@yahoo.com>
2018-05-06 14:13:32 -0400

    may061413

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+    res.render("home");
+});
+
+
+
+
+
+
+
+
+
+app.listen(3000, function(){
+    console.log('server on port 3000');
+});
