820942b3b07b088f0485beb82659e9ee8dbf1417
unknown <syyadkhan@yahoo.com>
2018-06-10 07:35:04 -0400

    replaced hard coded camps

--- app.js
+++ app.js
@@ -3,21 +3,45 @@ var app = express();
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 
+mongoose.connect("mongodb://localhost/khan_camp");
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-var campgrounds = [
-    {name: "big hill", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcReMI6FwL-HFdQFSWBWOwvQMAlBnJXpNIZIUTrmRp2J2TRrOT9L5w"},
-    {name: "small hill", image: "https://www.tourpackagejaisalmer.com/images/desert-night-camp.png"},
-    {name: "my hill", image: "http://www.rapidfitkc.com/wp-content/uploads/2017/09/Best-Boot-Camp-in-Kansas-city.jpg"}
-]
+//schema setup
+var campgroundSchema= new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create(
+//     {
+//         name: "small hill", 
+//         image: "https://www.tourpackagejaisalmer.com/images/desert-night-camp.png"
+// }, function(err, campground){
+//     if(err){
+//         console.log(err);
+//     }else{
+//         console.log("new camp:");
+//         console.log(campground);
+//     }
+// });
 
 app.get("/", function(req, res){
     res.render("landing");
 });
 
 app.get("/campgrounds", function(req, res){
-    res.render("campgrounds", {campgrounds: campgrounds});
+    //get camps from db
+    Campground.find({}, function(err, allCampgrounds){
+        if(err){
+            console.log(err);
+        }else{
+            res.render("campgrounds", {campgrounds: allCampgrounds});
+        }
+    });    
 });
 
 app.post("/campgrounds", function(req, res){
